45a1d87f205a43fb46803ca5d49b1e7bc557464f
kittoku <89574677+kittokku@users.noreply.github.com>
2025-07-18 19:45:10 +0800

    feat: add home page

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from routes.index import index_bp
 from routes.login import login_bp
 from routes.register import register_bp
+from routes.home import home_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(index_bp)
 app.register_blueprint(login_bp)
 app.register_blueprint(register_bp)
+app.register_blueprint(home_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
